bda4df1c8b7a5534d3254d4c773b2e303540d8ac
Jay Rovacsek <jay@rovacsek.com>
2022-08-11 09:45:21 +1000

    feat: create hello world content

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   name = "blog shell";
-  buildInputs = with pkgs; [ zola nixfmt statix ];
-  shellHook = "";
+  buildInputs = with pkgs; [ git zola nixfmt statix ];
+  shellHook = ''
+    git pull --recurse-submodules
+    zola serve --port 8080
+  '';
 }
